b2a689a693816065eda8771b6225689778f680ff
Alexey Golovnya <alexey.golovnya@gmail.com>
2016-05-20 07:32:25 +0600

    Add index.html to git

--- src/TestApp.elm
+++ src/TestApp.elm
@@ -5,12 +5,15 @@ import Billing.Model
 import Billing.Rest
 import Order.View
 
+resultToHtml : Result String Billing.Model.Packet -> Html.Html ()
 resultToHtml result =
   case result of
     Ok packet -> Order.View.packet packet
     Err message -> text message
 
+jsonchick : String
 jsonchick = """{"id":3447,"name":"Тест","type":{"alias":"kd","title":"Коммерческий доступ"},"client_type":"user","region":{"id":24,"name":"Кемеровская область"},"geo":{"id":0,"name":""},"project":{"id":37,"alias":"","name":""},"period":{"unit":"day","unitWordFormsRu":["день","дня","дней"],"range":{"min":30,"max":90,"step":30}},"activateDate":1361861795,"deactivateDate":0,"services":[{"alias":"offers_limit","title":"Лимит объявлений","visible":true,"changeable":false,"type":"int","defaultValue":200,"unit":"","unitWordFormsRu":["","",""],"min":0,"max":null}]}"""
 
+main : Html.Html ()
 main =
   div [] [ resultToHtml <| Json.Decode.decodeString Billing.Rest.decodePacket jsonchick ]
